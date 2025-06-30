add2b7fe601b7d06ad1044c9c748dacf4f706f6d
hieunv-0908 <nn650370@gmail.com>
2025-06-30 22:23:45 +0700

    ss7

--- ss7/PTIT_CNTT5_IT201_Session07_Bai08.c
+++ ss7/PTIT_CNTT5_IT201_Session07_Bai08.c
@@ -1,22 +1,52 @@
-//
-// Created by Admin on 17/06/2025.
-//
-#include "../common.h"
-
-int total_cach(int n);
+#include <stdio.h>
 
 int main() {
-    int n;
-    printf("moi nhap vao so bac:");
-    scanf("%d",&n);
-    while (getchar()!='\n');
-    printf("%d cach",total_cach(n));
+    int rows, cols;
+    printf("rows = ");
+    scanf("%d", &rows);
+    printf("cols = ");
+    scanf("%d", &cols);
+
+    if (rows <= 0 || rows >= 1000 || cols <= 0 || cols >= 1000) {
+        printf("Kích thước mảng không hợp lệ\n");
+        return 1;
+    }
+
+    int arr[1000][1000];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int k;
+    printf("k = ");
+    scanf("%d", &k);
+
+    if (k < 0 || k >= cols) {
+        printf("Chỉ số cột không hợp lệ\n");
+        return 1;
+    }
+
+    for (int i = 0; i < rows - 1; i++) {
+        for (int j = i + 1; j < rows; j++) {
+            if (arr[i][k] > arr[j][k]) {
+                for (int col = 0; col < cols; col++) {
+                    int temp = arr[i][col];
+                    arr[i][col] = arr[j][col];
+                    arr[j][col] = temp;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
-}
-
-int total_cach(int n) {
-    if (n == 0) return 1;
-    if (n<0) return 0;
-    return total_cach(n-1) + total_cach(n-2);
-
 }
--- ss7/PTIT_CNTT5_IT201_Session07_Bai09.c
+++ ss7/PTIT_CNTT5_IT201_Session07_Bai09.c
@@ -1,52 +1,59 @@
 #include <stdio.h>
 
-int main() {
-    int rows, cols;
-    printf("rows = ");
-    scanf("%d", &rows);
-    printf("cols = ");
-    scanf("%d", &cols);
+#define MAX_SIZE 1000
 
-    if (rows <= 0 || rows >= 1000 || cols <= 0 || cols >= 1000) {
-        printf("Kích thước mảng không hợp lệ\n");
+int main() {
+    int n;
+    printf("n = ");
+    scanf("%d", &n);
+
+    if (n <= 0 || n > MAX_SIZE) {
+        printf("Số lượng phần tử không hợp lệ\n");
         return 1;
     }
 
-    int arr[1000][1000];
+    int arr[MAX_SIZE], even[MAX_SIZE], odd[MAX_SIZE];
+    int i, evenCount = 0, oddCount = 0;
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            scanf("%d", &arr[i][j]);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        if (arr[i] % 2 == 0) {
+            even[evenCount++] = arr[i];
+        } else {
+            odd[oddCount++] = arr[i];
         }
     }
 
-    int k;
-    printf("k = ");
-    scanf("%d", &k);
-
-    if (k < 0 || k >= cols) {
-        printf("Chỉ số cột không hợp lệ\n");
-        return 1;
-    }
-
-    for (int i = 0; i < rows - 1; i++) {
-        for (int j = i + 1; j < rows; j++) {
-            if (arr[i][k] > arr[j][k]) {
-                for (int col = 0; col < cols; col++) {
-                    int temp = arr[i][col];
-                    arr[i][col] = arr[j][col];
-                    arr[j][col] = temp;
-                }
+    for (i = 0; i < evenCount - 1; i++) {
+        for (int j = i + 1; j < evenCount; j++) {
+            if (even[i] > even[j]) {
+                int temp = even[i];
+                even[i] = even[j];
+                even[j] = temp;
             }
         }
     }
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%d ", arr[i][j]);
+    for (i = 0; i < oddCount - 1; i++) {
+        for (int j = i + 1; j < oddCount; j++) {
+            if (odd[i] < odd[j]) {
+                int temp = odd[i];
+                odd[i] = odd[j];
+                odd[j] = temp;
+            }
         }
-        printf("\n");
     }
+
+    printf("[");
+    for (i = 0; i < evenCount; i++) {
+        printf("%d", even[i]);
+        if (i != n - 1) printf(", ");
+    }
+    for (i = 0; i < oddCount; i++) {
+        if (evenCount + i != 0) printf(", ");
+        printf("%d", odd[i]);
+    }
+    printf("]\n");
 
     return 0;
 }
