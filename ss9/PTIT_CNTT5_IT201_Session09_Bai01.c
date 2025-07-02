c036f3efdbe86a61b6b89c9409701f9364242993
hieunv-0908 <nn650370@gmail.com>
2025-07-03 01:17:02 +0700

    ss9

--- ss9/PTIT_CNTT5_IT201_Session09_Bai01.c
+++ ss9/PTIT_CNTT5_IT201_Session09_Bai01.c
@@ -1,15 +1,37 @@
 #include "../common.h"
-    struct NODE {
-        int data;
-        struct NODE *next;
-    };
+struct NODE {
+    int data;
+    struct NODE *next;
+};
+
+struct NODE* create_node(int data) {
+    struct NODE* new_node = (struct NODE*)malloc(sizeof(struct NODE));
+    new_node->data = data;
+    new_node->next = NULL;
+    return new_node;
+}
+
+void append_node(struct NODE** head,int value) {
+    struct NODE* new_node = create_node(value);
+    if (*head == NULL) {
+        *head = new_node;
+        return;
+    }
+    struct NODE* current = *head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = new_node;
+}
+
 int main() {
     struct NODE *head = NULL;
-    struct NODE *elemen1 = NULL;
-    struct NODE *elemen2 = NULL;
-    struct NODE *elemen3 = NULL;
-    struct NODE *elemen4 = NULL;
-    struct NODE *elemen5 = NULL;
-
+    append_node(&head,1);
+    for (int i = 0; i < 6; i++) {
+        printf("Nhap vao gia tri cua phan tu so %d cua danh sach:",i+1);
+        int value;
+        scanf("%d", &value);
+        append_node(&head,value);
+    }
     return 0;
 }
