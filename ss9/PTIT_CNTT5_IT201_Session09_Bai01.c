fd3d6ce89f4c55309ccd8a8285a91a648141d062
hieunv-0908 <nn650370@gmail.com>
2025-07-03 00:05:02 +0700

    ss9

---
+++ ss9/PTIT_CNTT5_IT201_Session09_Bai01.c
@@ -0,0 +1,15 @@
+#include "../common.h"
+    struct NODE {
+        int data;
+        struct NODE *next;
+    };
+int main() {
+    struct NODE *head = NULL;
+    struct NODE *elemen1 = NULL;
+    struct NODE *elemen2 = NULL;
+    struct NODE *elemen3 = NULL;
+    struct NODE *elemen4 = NULL;
+    struct NODE *elemen5 = NULL;
+
+    return 0;
+}
