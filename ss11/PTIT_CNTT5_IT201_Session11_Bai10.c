93199fad826083b3d178bbf639f732a477aa6d45
hieunv-0908 <nn650370@gmail.com>
2025-07-06 23:39:05 +0700

    ss11

--- ss11/PTIT_CNTT5_IT201_Session11_Bai10.c
+++ ss11/PTIT_CNTT5_IT201_Session11_Bai10.c
@@ -1,4 +1,5 @@
 #include "../common.h"
+
 struct NODE {
     int data;
     struct NODE *next;
@@ -69,69 +70,4 @@ void delete_at_position(struct NODE** head, int position) {
         return;
     }
 
-    while (curr != NULL && index < position) {
-        curr = curr->next;
-        index++;
-    }
-
-    if (curr == NULL) return;
-
-    if (curr->prev) curr->prev->next = curr->next;
-    if (curr->next) curr->next->prev = curr->prev;
-    free(curr);
-}
-
-void reverse_list(struct NODE** head) {
-    struct NODE* curr = *head;
-    struct NODE* temp = NULL;
-
-    while (curr != NULL) {
-        temp = curr->prev;
-        curr->prev = curr->next;
-        curr->next = temp;
-        curr = curr->prev;
-    }
-
-    if (temp != NULL)
-        *head = temp->prev;
-}
-
-void print_list(struct NODE* head) {
-    struct NODE* curr = head;
-    printf("Danh sach: ");
-    while (curr != NULL) {
-        printf("%d", curr->data);
-        if (curr->next != NULL) printf("<->");
-        curr = curr->next;
-    }
-    printf("->NULL\n");
-}
-
-int main() {
-    struct NODE* head = NULL;
-    int values[] = {1, 2, 3, 4, 5};
-    for (int i = 0; i < 5; i++)
-        append_node(&head, values[i]);
-
-    printf("Ban dau:\n");
-    print_list(head);
-
-    int val, pos;
-    printf("\nNhap gia tri muon chen: ");
-    scanf("%d", &val);
-    printf("Nhap vi tri muon chen (bat dau tu 0): ");
-    scanf("%d", &pos);
-    insert_at_position(&head, val, pos);
-    print_list(head);
-
-    printf("\nNhap vi tri muon xoa (bat dau tu 0): ");
-    scanf("%d", &pos);
-    delete_at_position(&head, pos);
-    print_list(head);
-
-    printf("\nDao nguoc danh sach:\n");
-    reverse_list(&head);
-    print_list(head);
-
-    return 0;
-}
+    while (curr != NULL &&
