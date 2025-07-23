e31bb157f9a90382591ef350c8d97d68221a2d2a
hieunv-0908 <nn650370@gmail.com>
2025-07-24 00:01:33 +0700

    bai21_dothi

---
+++ bai21_dothi/PTIT_CNTT5_IT201_Session21_Bai03.c
@@ -0,0 +1,31 @@
+//
+// Created by Admin on 23/07/2025.
+//
+#include <stdio.h>
+#define MAX 100
+
+void addEdge(int graph[][MAX], int u, int v) {
+    graph[u][v] = 1;
+}
+
+void printGraph(int graph[][MAX], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", graph[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n = 4;
+    int graph[MAX][MAX] = {0};
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 2);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 2, 0);
+
+    printGraph(graph, n);
+    return 0;
+}
